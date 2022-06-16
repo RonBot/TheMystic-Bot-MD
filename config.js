138053f0ff93ff3bffb00f3987337adc1498e318
Roncito <96086954+RonBot@users.noreply.github.com>
2022-06-16 16:50:42 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,8 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['50575008120', '🤖 Mystic - Bot 🤖', true],
-  ['17722386341']
+  ['573023900508', '👑 Kanixx - Creador 👑', true],
+  ['37122262180', '🤖 Kan - Bot 🤖', true]
 ] // Cambia los numero que quieras
 global.mods = [] 
 global.prems = [] 
